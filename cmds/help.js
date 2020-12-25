e334d9f26f17478b6d3cc451713911824094b86c
Naufal-DevVoder <75933350+Naufal-DevVoder@users.noreply.github.com>
2020-12-25 20:46:08 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -5,9 +5,9 @@ module.exports.run = async(client,message,args) => {
 
 
 let embed = new Discord.MessageEmbed()
-.addField("General Commands",`${client.commands.filter(command => command.help.category == "general").map(e => `\`${e.help.name}\``).join(",")}`)
-.addField("Moderation Commands",`${client.commands.filter(command => command.help.category == "moderation").map(e => `\`${e.help.name}\``).join(",")}`)
-.addField("Fun Commands",`${client.commands.filter(command => command.help.category == "fun").map(e => `\`${e.help.name}\``).join(",")}`)
+.addField("General Commands",`${client.commands.filter(command => command.help.category == "general").map(e => `\`${e.help.name}\```).join(", ")}`)
+.addField("Moderation Commands",`${client.commands.filter(command => command.help.category == "moderation").map(e => `\`${e.help.name}\```).join(", ")}`)
+.addField("Fun Commands",`${client.commands.filter(command => command.help.category == "fun").map(e => `\`${e.help.name}\```).join(", ")}`)
 .setFooter(`${client.user.username} Help Command`,client.user.avatarURL())
 .setTitle(`${client.user.username} Commands`)
 .setURL("https://discord.gg/PE6R7p6")
